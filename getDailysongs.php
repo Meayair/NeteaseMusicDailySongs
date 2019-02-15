f61184950013af9d8b1f3a84e92140adcd12f180
大米 <38695183+Meayair@users.noreply.github.com>
2019-02-15 14:05:53 +0800

    重整一些数据格式

--- getDailysongs.php
+++ getDailysongs.php
@@ -4,11 +4,20 @@ use Metowolf\Meting;
 $api = new Meting('netease');
 @$songslist = file_get_contents("./DailySongs.json");
 $songslist = json_decode($songslist,true);
-if(!isset($songslist[$date]))
+$date = date("Ymd",time()-21600);
+$i=0;
+foreach ($songslist as $value) {
+    if($value['date']==$date)
+    $i=1;
+}
+if(!$i)
 {
 $cookie = '';//此处填写自己的cookie 如何获取cookie：https://www.meayair.com/archives/33.html
 $song = $api->cookie($cookie)->dailysongs();
-$date = date("Ymd",time()-21600);
-$songslist[$date] = $song;
+$songslist[] = array(
+    "list" =>$song,
+    "date" =>$date
+    );
 }
+rsort($songslist);
 if(file_put_contents("./DailySongs.json",json_encode($songslist)))echo "ok";
